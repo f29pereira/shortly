49635b513f20c605b045f88fa304552bf5c924a9
Filipe Pereira <f29.pereira@gmail.com>
2025-11-06 14:21:00 +0000

    Next.js installation, Add font, colors and images
    - Add images to be used in the project
    - Add styling to globals.css
    - Add font: Poppins

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["500", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Shortly",
+  description: "Frontend Mentor: URL shortening API landing page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable}`}>{children}</body>
+    </html>
+  );
+}
