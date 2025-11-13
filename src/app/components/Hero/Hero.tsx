5b2467105af0754764054bf0d5d9ad204767fbdd
Filipe Pereira <f29.pereira@gmail.com>
2025-11-13 17:22:40 +0000

    Add Hero Section
    - Update globals.css styling
    - Add Hero component that renders the hero section
    - Update Home component with Hero

---
+++ src/app/components/Hero/Hero.tsx
@@ -0,0 +1,44 @@
+import styles from "./Hero.module.css";
+import Image from "next/image";
+import PillLink from "../PillLink/PillLink";
+
+/**
+ * Renders the hero section with:
+ * - Image of a person working
+ * - Title
+ * - Descripton of Shortly
+ * - Get started link
+ */
+export default function Hero() {
+  return (
+    <section className={styles.heroCont}>
+      {/*Hero Image*/}
+      <div className={styles.imgCont}>
+        <Image
+          className={styles.img}
+          src="images/illustration-working.svg"
+          width={733}
+          height={482}
+          alt=""
+        />
+      </div>
+
+      {/*Hero content*/}
+      <div className={styles.contentCont}>
+        {/*Title*/}
+        <h1 className={styles.title}>More than just shorter links</h1>
+
+        {/*Descripton of Shortly*/}
+        <p className={styles.description}>
+          Build your brand&apos;s recognition and get detailed insights on how
+          your links are performing.
+        </p>
+
+        {/*Get started link*/}
+        <div className={styles.getStartedCont}>
+          <PillLink description="Get Started" href="#" />
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "./components/Hero/Hero";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 }
